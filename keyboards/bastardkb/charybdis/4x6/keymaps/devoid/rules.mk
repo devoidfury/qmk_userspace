b68ae4a33089761faa138d8455a9f97effe5205f
tom hunkapiller <tom@furycodes.com>
2026-03-04 23:06:47 +0000

    reorganize rules

--- keyboards/bastardkb/charybdis/4x6/keymaps/devoid/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/keymaps/devoid/rules.mk
@@ -4,16 +4,12 @@ VIA_ENABLE = no
 # leader key, QK_LEAD
 LEADER_ENABLE = yes
 
+SEND_STRING_ENABLE = yes
+
 # unused features turned off to save space
 SPACE_CADET_ENABLE = no
 COMBO_ENABLE = no
 GRAVE_ESC_ENABLE = no
-
-ifdef LEADER_ENABLE
-SEND_STRING_ENABLE = yes
-else
-SEND_STRING_ENABLE = no
-endif
 
 # link time optimization to save space
 LTO_ENABLE = yes
